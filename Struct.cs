0e0e7a6a7f29bfe9244b606dd96e6ee4ce56aa64
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-07-29 22:09:02 -0300

    refactor: syntax correction

--- Struct.cs
+++ Struct.cs
@@ -1,73 +1,48 @@
-using System;
+//STRUCTS
 
+    /*
+        DEFINIÇÕES
+
+        TIPOS DE DADO ESTRUTURADO
+        APENAS A ESTRUTURA, O ESQUELETO
+        VALUE TYPE
+        ARMAZENAM APENAS OUTROS TIPOS DE DADOS
+        DEFINIDO PELA PALAVRA STRUCT
+        COMPOSTO DE PROPRIEDADES E MÉTODOS
+        NOME SEMPRE COM MAIÚSCULO
+            - O MESMO PARA PROPRIEDADES E MÉTODOS
+        CRIADO A PARTIR DA PALAVRA NEW
+            - NESTE MOMENTO SIM TEMOS OS VALORES
+    */
+    
 namespace Teste
 {
     public class Struct
     {
-        //ISTÂNCIANDO
-        static void PriceProduct(){
-            var Product = new Product();
+        
+    }
 
-            Product.Id = 1;
-            Product.Title = "Teclado PRO.";
-            Product.Price = 130.99f;
+    public struct Products{
+        public string Title;
+        public double Price;
+        public string Description;
 
-            Console.WriteLine(Product.Id);
-            Console.WriteLine(Product.Title);
-            Console.WriteLine(Product.Price);
-        }
-
-        //ISTÂNCIANDO
-        static void CarIten(){
-            var Car = new Car("Ford",2015, true);
-
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
+        public double PriceInDolar(double dolar){
+            return Price * dolar;
         }
     }
 
-    //STRUCTS
 
-        /*
-            DEFINIÇÕES
-
-            TIPOS DE DADO ESTRUTURADO
-            APENAS A ESTRUTURA, O ESQUELETO
-            VALUE TYPE
-            ARMAZENAM APENAS OUTROS TIPOS DE DADOS
-            DEFINIDO PELA PALAVRA STRUCT
-            COMPOSTO DE PROPRIEDADES E MÉTODOS
-            NOME SEMPRE COM MAIÚSCULO
-                - O MESMO PARA PROPRIEDADES E MÉTODOS
-            CRIADO A PARTIR DA PALAVRA NEW
-                - NESTE MOMENTO SIM TEMOS OS VALORES
-        */
-        
-        //A ANATOMIA DE UMA STRUCT NORMAMAENTE É:
-        struct Product
+    public struct Users{
+        public Users(string name, byte age, string nationality)
         {
-            // Propriedades
-            public int Id;
-            public float Price;
-            public string Title;
+            Name = name;
+            Age = age;
+            Nationality = nationality;
 
-            // Métodos
-            public float PriceInDolar(float dolar){
-                return Price * dolar;
-            }
-
-        };
-
-        //APLICANDO MÉTODO CONSTRUTOR
-
-        struct Car
-        {
-            public Car(string mark, int model, bool combustion)
-            {
-                var Mark = mark;
-                var Model = model;
-                var Combustion = combustion;
-            }
         }
+        public string Name;
+        public byte Age;
+        public string Nationality;
+    }
 }
